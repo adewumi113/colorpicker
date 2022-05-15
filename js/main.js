216915d97130d1c1b5a21eeb46b75aa90da1b4a4
adewumi113 <104263441+adewumi113@users.noreply.github.com>
2022-05-14 19:36:44 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.getElementById('purple').onclick = bigPurple
+document.getElementById('green').onclick = fakeGreen
+document.getElementById('blue').onclick = satoshiBlue
+
+function bigPurple() {
+document.querySelector('body').style.backgroundColor = 'yellow'
+document.querySelector('body').style.Color = 'white'
+}
+
+function fakeGreen() {
+document.querySelector('body').style.backgroundColor = 'red'
+document.querySelector('body').style.Color = 'white'
+}
+
+function satoshiBlue() {
+document.querySelector('body').style.backgroundColor = 'orange'
+document.querySelector('body').style.Color = 'white'
+}
